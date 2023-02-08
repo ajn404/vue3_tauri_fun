530bb55587177419c5ab1aead771f78982b85eb6
mac_ajn404 <huiyuening00@gmail.com>
2023-02-08 18:19:56 +0800

    add unplugin-vue-components vant element-plus and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,43 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import Components from "unplugin-vue-components/vite";
+import {
+  ElementPlusResolver,
+  AntDesignVueResolver,
+  VantResolver,
+  HeadlessUiResolver,
+  ElementUiResolver,
+} from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      // ui库解析器，也可以自定义
+      resolvers: [
+        ElementPlusResolver(),
+        AntDesignVueResolver(),
+        VantResolver(),
+        HeadlessUiResolver(),
+        ElementUiResolver(),
+      ],
+      dirs: ["src/components"],
+      // ui库解析器
+      // resolvers: [ElementPlusResolver()],
+      extensions: ["vue"],
+      // 配置文件生成位置
+      dts: "src/components.d.ts",
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
