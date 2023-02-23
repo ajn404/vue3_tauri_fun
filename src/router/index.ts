59d9966566b1e76834a001adb7082d11fff4a86b
mac_ajn404 <huiyuening00@gmail.com>
2023-02-23 10:23:07 +0800

    vite路由自动化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
+//vite路由自动化
 const routes = import.meta.glob('@/views/routes/**/*.vue');
+const endRoutes = Object.keys(routes).map((path) => {
+  const name = path.match(/\/views\/routes\/(.*)\.vue$/)[1];   
+  const indexName = path.match(/\/views\/routes\/(.*)index\.vue$/);
+  const endPath = indexName?indexName[1]:name;
+  return {
+    path: `/${endPath}`,
+    name,
+    component: routes[path],
+  };
+});
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: Object.keys(routes).map((path) => {
-    const name = path.match(/\/views\/routes\/(.*)\.vue$/)[1];   
-    console.log(name);
-     
-    return {
-      path: `/${name}`,
-      name,
-      component: routes[path],
-    };
-  }),
+  routes:endRoutes
 });
 
-console.log(routes);
+console.log(router);
 
 //this code is providing error
 //handling for the replace method
