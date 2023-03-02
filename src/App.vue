bff112be315f3d2d9e091c87861ef48ca1cf02ab
mac_ajn404 <huiyuening00@gmail.com>
2023-03-02 17:35:52 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,9 @@
 <script setup lang="ts" name="App">
-import { RouterView, useRoute } from 'vue-router';
-import { ref, watch,getCurrentInstance } from "vue";
-const transitionName = ref("");
-const route = useRoute();
-const app = getCurrentInstance();
-console.log(app);
-
-
-watch(
-  route,
-  (to, from) => {
-    const toDepth = to.path.split('/').length
-    const fromDepth = from.path.split('/').length
-    if (toDepth === fromDepth) {
-      transitionName.value = ''
-    } else {
-      transitionName.value = toDepth < fromDepth ? 'slide-right' : 'slide-left'
-    }
-  })
-
-  // if (sessionStorage.getItem('store')) {
-  //     this.$store.replaceState(
-  //       Object.assign(
-  //         {},
-  //         this.$store.state,
-  //         JSON.parse(sessionStorage.getItem('store')
-  //         )
-  //       )
-  //     )
-  //   }
-
-
-    //在页面刷新时将vuex里的信息保存到sessionStorage里
-    // window.addEventListener('beforeunload', () => {
-    //   sessionStorage.setItem('store', JSON.stringify(this.$store.state))
-    // })
-
-
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <transition :name="transitionName">
+  <transition>
     <keep-alive>
       <RouterView />
     </keep-alive>
