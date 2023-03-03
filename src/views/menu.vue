1cf909b29b6acab78e1b096e88bff7782dc0ddae
mac_ajn404 <huiyuening00@gmail.com>
2023-03-03 18:20:07 +0800

    https://codepen.io/erikterwan/pen/EVzeRP

---
+++ src/views/menu.vue
@@ -0,0 +1,28 @@
+<!-- https://codepen.io/erikterwan/pen/EVzeRP -->
+<template>
+    <nav role="navigation">
+        <div id="menuToggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+                <a href="#">
+                    <li>Home</li>
+                </a>
+                <a href="#">
+                    <li>About</li>
+                </a>
+                <a href="#">
+                    <li>Info</li>
+                </a>
+                <a href="#">
+                    <li>Contact</li>
+                </a>
+                <a href="https://erikterwan.com/" target="_blank">
+                    <li>Show me more</li>
+                </a>
+            </ul>
+        </div>
+    </nav>
+</template>
