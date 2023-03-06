befc47050820ecfdf46c17c531d83c91223b0fae
mac_ajn404 <huiyuening00@gmail.com>
2023-03-06 18:14:39 +0800

    成功引入three

--- src/views/routes/three/index.vue
+++ src/views/routes/three/index.vue
@@ -1,3 +1,19 @@
 <template>
-    <div></div>
+    <div ref="container"></div>
 </template>
+
+<script lang="ts" setup>
+import Sketch from "@/script/threeSketch";
+import { ref, nextTick } from "vue";
+const container = ref(null);
+
+let sketch = new Sketch();
+
+nextTick(() => {
+    const options = {
+        container: container.value || document.body
+    };
+    sketch.init(options);
+
+})
+</script>
