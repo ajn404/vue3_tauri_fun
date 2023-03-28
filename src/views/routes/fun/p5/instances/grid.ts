27abb0bcd5d9ae2a9bbda0a1effb0c81fe5e0d31
mac_ajn404 <huiyuening00@gmail.com>
2023-03-28 18:15:21 +0800

    ..

--- src/views/routes/fun/p5/instances/grid.ts
+++ src/views/routes/fun/p5/instances/grid.ts
@@ -1,35 +1,44 @@
 import type p5 from 'p5';
 
-const make2DArray = (cols: any, rows: any, w: number, h: number, _: p5) => {
+const make2DArray = (
+  cols: number,
+  rows: number,
+  w: number,
+  h: number,
+  sketch: p5
+) => {
   const arr = new Array(cols);
-  for (let i = 0; i < cols; i++) {
+  for (let i = 1; i < cols - 1; i += 2) {
     arr[i] = new Array(rows);
-    for (let j = 0; j < cols; j++) {
-      _.rect(i * w, j * h, w, h);
+    for (let j = 1; j < rows - 1; j += 2) {
+      sketch.rect(i * w, j * h, w, h);
+      arr[i][j] = {
+        x: i * w,
+        y: j * h,
+      };
     }
   }
   return arr;
 };
 //coding is art
-export const grid = (_: p5, container: HTMLElement | null) => {
+export const grid = (sketch: p5, container: HTMLElement | null) => {
   const rows = 20,
     cols = 20;
   let w = 20,
     h = 20;
 
-  _.setup = () => {
-    _.createCanvas(
+  sketch.setup = () => {
+    sketch.createCanvas(
       container?.clientWidth || 400,
       container?.clientHeight || 400
     );
-    _.background(0).noStroke();
-    w = _.width / cols;
-    h = _.height / rows;
-    console.log(w, h);
+    sketch.background(0).noStroke();
+    w = sketch.width / cols;
+    h = sketch.height / rows;
 
-    const grid = make2DArray(cols, rows, w, h, _);
+    const grid = make2DArray(cols, rows, w, h, sketch);
     console.log(grid);
   };
 
-  _.draw = () => {};
+  sketch.draw = () => {};
 };
