bbb44b26aec06838cb9541e9a204d37464e8013b
mac_ajn404 <huiyuening00@gmail.com>
2023-03-29 17:54:54 +0800

    no message

--- src/views/routes/three/basic/scene.ts
+++ src/views/routes/three/basic/scene.ts
@@ -1,7 +1,7 @@
-import { Color, Scene } from 'three';
+import { Scene } from 'three';
 
 export const createScene = () => {
   const scene = new Scene();
-  scene.background = new Color('rgba(10,10,10,0.5)');
+  //   scene.background = new Color('rgba(10,10,10,0.5)');
   return scene;
 };
