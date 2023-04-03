814d4547c4557f8d829b71d495eb8da8b6eeed51
mac_ajn404 <huiyuening00@gmail.com>
2023-04-03 11:05:06 +0800

    CAN'T SOLVE IN P5.SOUND IN CHROME MAYBE I SHOULD ABANDON

--- src/views/routes/fun/p5/instances/sound/piano.ts
+++ src/views/routes/fun/p5/instances/sound/piano.ts
@@ -1,31 +1,54 @@
 import type p5 from 'p5';
-import C2Path from "@/assets/piano/C2.mp3"
+import C2Path from '@/assets/piano/C2.mp3';
 import type { SoundFile } from 'p5';
-import type { p5_func } from '../index'
+import type { p5_func } from '../index';
+// import { getBrowserVersion } from '@/script/utils';
+
 const all: p5_func = (sketch: p5, container: HTMLElement | null) => {
-    let C2: SoundFile | null = null;
+  let C2: SoundFile | null = null;
 
-    const preload = async () => {
-        await import('p5/lib/addons/p5.sound');
-        sketch.soundFormats('mp3');
-        C2 = sketch.loadSound(C2Path);
-    }
-    sketch.setup = async () => {
-        sketch.createCanvas(
-            container?.clientWidth || 400,
-            container?.clientHeight || 400,
-            sketch.WEBGL
-        );
-        sketch.background(220);        
-        await preload();
-        sketch.mousePressed = mousePressed;
-    };
+  const preload = async () => {
+    const sound = await import('p5/lib/addons/p5.sound');
+    sketch.soundFormats('mp3');
+    console.log(sound);
 
-    const mousePressed = () => {
-        // console.log('start');
+    C2 = sketch.loadSound(C2Path);
+  };
+  sketch.setup = async () => {
+    await preload();
+    sketch.createCanvas(
+      container?.clientWidth || 400,
+      container?.clientHeight || 400,
+      sketch.WEBGL
+    );
 
-        C2?.play();
-    }
-}
+    sketch.background(220);
+
+    // renderer.mousePressed(mousePressed);
+  };
+
+  document.addEventListener('click', async () => {
+    // const mic = sketch.getAudioContext();
+    // await mic?.audioWorklet.addModule(
+    //   'node_modules/.vite/deps/p5_lib_addons_p5__sound.js'
+    // );
+    sketch.userStartAudio();
+    C2?.play();
+  });
+
+  //   const mousePressed = async () => {
+  //     const { browser } = getBrowserVersion();
+
+  //     if (browser === 'chrome') {
+  //       console.log(sketch);
+
+  //       const context = new AudioContext();
+  //       await context.audioWorklet.addModule('p5/lib/addons/p5.sound');
+  //       new AudioWorkletNode(context, 'worklet-processor');
+  //     }
+  //     sketch.userStartAudio();
+  //     C2?.play();
+  //   };
+};
 
 export const piano = all;
