c7e5b2e9fcd60c19da13aa7aeb0603424ef53310
ajn404 <huiyuening00@gmail.com>
2023-04-03 10:10:31 +0800

    Submit:still error

--- src/views/routes/fun/p5/instances/sound/piano.ts
+++ src/views/routes/fun/p5/instances/sound/piano.ts
@@ -4,27 +4,26 @@ import type { SoundFile } from 'p5';
 import type { p5_func } from '../index'
 const all: p5_func = (sketch: p5, container: HTMLElement | null) => {
     let C2: SoundFile | null = null;
-    sketch.preload = async () => {
-        await import('p5/lib/addons/p5.sound')
 
+    const preload = async () => {
+        await import('p5/lib/addons/p5.sound');
         sketch.soundFormats('mp3');
         C2 = sketch.loadSound(C2Path);
     }
-    sketch.setup = () => {
+    sketch.setup = async () => {
         sketch.createCanvas(
             container?.clientWidth || 400,
             container?.clientHeight || 400,
             sketch.WEBGL
         );
-
-        sketch.background(220);
-        // C2?.play();
-
+        sketch.background(220);        
+        await preload();
+        sketch.mousePressed = mousePressed;
     };
 
-    sketch.mousePressed = () => {
-        console.log('start');
-        
+    const mousePressed = () => {
+        // console.log('start');
+
         C2?.play();
     }
 }
