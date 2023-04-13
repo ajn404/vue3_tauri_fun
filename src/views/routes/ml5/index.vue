af36e0372ea491ba39ccd68c3da15893681483e9
ajn404 <huiyuening00@gmail.com>
2023-04-13 11:56:13 +0800

    Submit:

--- src/views/routes/ml5/index.vue
+++ src/views/routes/ml5/index.vue
@@ -1,86 +1,109 @@
 <template>
-  <div class="view">
-    <canvas ref="canvas" width="640" height="480"></canvas>
-    <video ref="video" width="640" height="480" class="hidden"></video>
+  <div class="view" ref="view">
+    <canvas ref="canvas"> </canvas>
+    <video ref="video" class="hidden"></video>
   </div>
 </template>
 
 <script lang="ts" setup>
 import * as ml5 from 'ml5';
-import { onMounted, ref, type Ref } from 'vue';
+import { onMounted, onUnmounted, ref, type Ref } from 'vue';
 //这个包着实有点大
 const canvas: Ref<HTMLCanvasElement | null> = ref(null);
 const video: Ref<HTMLVideoElement | null> = ref(null);
+const view: Ref<HTMLElement | null> = ref(null);
 
-onMounted(() => {
-  // Create a webcam capture
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(function (stream) {
-        if (video.value) {
-          video.value.srcObject = stream;
-          video.value.play();
-        }
-      });
-  }
 
-  if (canvas.value && video.value) {
-    const ctx: CanvasRenderingContext2D | null = canvas.value.getContext('2d');
-    let poses: any[] = [];
+const animationId = ref(0);
 
-    function drawCameraIntoCanvas() {
-      if (video.value && ctx) ctx.drawImage(video.value, 0, 0, 640, 480);
-      drawKeypoints();
-      drawSkeleton();
+onUnmounted(() => {
+  cancelAnimationFrame(animationId.value)
+}),
 
-      requestAnimationFrame(drawCameraIntoCanvas);
+  onMounted(() => {
+    // Create a webcam capture
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function (stream) {
+          if (video.value) {
+            console.log('1');
+
+            video.value.srcObject = stream;
+            video.value.play();
+          }
+        });
     }
-    drawCameraIntoCanvas();
 
-    const poseNet = ml5.poseNet(video.value, () => {
-      poseNet.multiPose(video.value);
-    });
+    if (canvas.value && video.value) {
+      let ctx: CanvasRenderingContext2D | null = canvas.value.getContext('2d');
+      let poses: any[] = [];
 
-    poseNet.on('pose', (res: any[]) => {
-      poses = res;
-    });
+      function drawCameraIntoCanvas() {
+        // console.log(poses);
+        if (video.value && view.value && canvas.value) {
+          canvas.value.width = view.value.clientWidth;
+          canvas.value.height = view.value.clientHeight;
+          video.value.width = view.value.clientWidth;
+          video.value.height = view.value.clientHeight;
+          ctx = canvas.value.getContext('2d');
+          if (ctx) {
+            ctx.drawImage(video.value, 0, 0, view.value.clientWidth, view.value.clientHeight);
+            drawKeypoints();
+            drawSkeleton();
+          }
+        }
+        animationId.value = requestAnimationFrame(drawCameraIntoCanvas);
+      }
+      drawCameraIntoCanvas();
 
-    function drawKeypoints() {
-      if (ctx) {
-        for (let i = 0; i < poses.length; i += 1) {
-          for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
-            let keypoint = poses[i].pose.keypoints[j];
-            if (keypoint.score > 0.2) {
+      const poseNet = ml5.poseNet(video.value, () => {
+        poseNet.multiPose(video.value);
+      });
+
+      poseNet.on('pose', (res: any[]) => {
+        poses = res;
+      });
+
+      function drawKeypoints() {
+        if (ctx) {
+          for (let i = 0; i < poses.length; i += 1) {
+            for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
+
+              let keypoint = poses[i].pose.keypoints[j];
+              if (keypoint.score > 0.2) {
+                let { x, y } = keypoint.position;
+
+                ctx.beginPath();
+                ctx.arc(
+                  x,
+                  y,
+                  10,
+                  0,
+                  2 * Math.PI
+                );
+                ctx.fillText(keypoint.part, x - 10, y - 10)
+                ctx.stroke();
+              }
+            }
+          }
+        }
+      }
+
+      function drawSkeleton() {
+        if (ctx) {
+          for (let i = 0; i < poses.length; i += 1) {
+            for (let j = 0; j < poses[i].skeleton.length; j += 1) {
+              let partA = poses[i].skeleton[j][0];
+              let partB = poses[i].skeleton[j][1];
               ctx.beginPath();
-              ctx.arc(
-                keypoint.position.x,
-                keypoint.position.y,
-                10,
-                0,
-                2 * Math.PI
-              );
+              ctx.moveTo(partA.position.x, partA.position.y);
+              ctx.lineTo(partB.position.x, partB.position.y);
               ctx.stroke();
             }
           }
         }
       }
     }
-
-    function drawSkeleton() {
-      if (ctx) {
-        for (let i = 0; i < poses.length; i += 1) {
-          for (let j = 0; j < poses[i].skeleton.length; j += 1) {
-            let partA = poses[i].skeleton[j][0];
-            let partB = poses[i].skeleton[j][1];
-            ctx.beginPath();
-            ctx.moveTo(partA.position.x, partA.position.y);
-            ctx.lineTo(partB.position.x, partB.position.y);
-            ctx.stroke();
-          }
-        }
-      }
-    }
-  }
-});
+  });
 </script>
