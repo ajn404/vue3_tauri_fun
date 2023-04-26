b508408d11bab062f601ea4254d868d8f1e8b16e
mac_ajn404 <huiyuening00@gmail.com>
2023-04-26 16:41:42 +0800

    no message

--- src/views/routes/frontend/animationSlider.vue
+++ src/views/routes/frontend/animationSlider.vue
@@ -18,32 +18,60 @@ const imgs: Ref<null | HTMLCollectionOf<Element>> = ref(null);
 const maxDelta = window.innerWidth / 2;
 let nextPercentageUnconstrained = 0;
 let nextPercent = 0;
+let aid = 0;
 const mouseDown = (e: any) => {
     dataset.mouseDownAt = e.clientX;
 }
 const mouseUp = () => {
     dataset.mouseDownAt = '0';
     dataset.prevPercentage = dataset.percent;
+    cancelAnimationFrame(aid);
 }
+
+/**
+ * 
+ * @param e 
+ * Based on the code you provided, it seems that the mouse animation in animationSlider.vue is not as smooth as you would like it to be.
+
+One possible reason for this could be the use of the animate() method in the moveFun() function. The animate() method can be resource-intensive and may cause performance issues, especially when used repeatedly in a short amount of time
+ * 
+ */
 const moveFun = (e: any) => {
+    if (dataset.mouseDownAt === '0') return;
+
     if (track.value && imgs.value) {
         const mouseDelta = parseFloat(dataset.mouseDownAt) - e.clientX;
         nextPercentageUnconstrained = parseFloat(dataset.prevPercentage) + (mouseDelta / maxDelta) * -100;
         nextPercent = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
         dataset.percent = nextPercent;
-        track.value.animate({
-            transform: `translate(${nextPercent}%, -50%)`
-        }, { fill: "forwards", duration: 1200 });
+        track.value.style.transform = `translate(${nextPercent}%, -50%)`;
         for (const image of imgs.value) {
-            image.animate({
-                objectPosition: `${100 + nextPercent}% center`
-            }, { duration: 1200, fill: "forwards" });
+            (image as HTMLImageElement).style.objectPosition = `${100 + nextPercent}% center`;
         }
+        aid = requestAnimationFrame(() => moveFun(e));
 
     }
 }
+// const moveFun = (e: any) => {
+//     if (dataset.mouseDownAt === '0') return;
+//     if (track.value && imgs.value) {
+//         const mouseDelta = parseFloat(dataset.mouseDownAt) - e.clientX;
+//         nextPercentageUnconstrained = parseFloat(dataset.prevPercentage) + (mouseDelta / maxDelta) * -100;
+//         nextPercent = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+//         dataset.percent = nextPercent;
+//         track.value.animate({
+//             transform: `translate(${nextPercent}%, -50%)`
+//         }, { fill: "forwards", duration: 1200 });
+//         for (const image of imgs.value) {
+//             image.animate({
+//                 objectPosition: `${100 + nextPercent}% center`
+//             }, { duration: 1200, fill: "forwards" });
+//         }
+
+//     }
+// }
+
 const mouseMove = (e: any) => {
-    if (dataset.mouseDownAt === '0') return;
     moveFun(e)
 }
 
@@ -97,8 +125,9 @@ $track-width: (
         user-select: none;
         transform: translate(0, -50%
         );
-    animation-duration: 1200ms;
+    animation-duration: 20ms;
     transition-duration: 1200ms;
+
     animation-fill-mode: forwards;
     width: $track-width;
 
@@ -107,6 +136,10 @@ $track-width: (
         height: 56vh;
         object-fit: cover;
         object-position: 100% 50%;
+        animation-duration: 1200ms;
+        animation-fill-mode: forwards;
+        transition-duration: 1200ms;
+        transition: all 1200ms;
         aspect-ratio: 16/9;
     }
 }
